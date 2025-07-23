5e8b7144041ff4bcf7d1cfeb6efa814e4cd4d426
Rob <robin.besson1@gmail.com>
2025-07-23 17:37:51 +0200

    changes for deploy on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static assets (e.g., CSS, images)
 app.use('/css', express.static(path.join(__dirname, 'css')));
