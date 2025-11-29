ad4fed8e31e6f21decfc768f49cb97173a29fc8e
Bourdat Loïc <bourdatloic@MacBook-Pro-de-Bourdat.local>
2025-11-29 18:47:51 +0100

    Atomisation initiale

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,40 +12,79 @@ app.use('/img', express.static(path.join(__dirname, 'img')));
 app.use('/sitemap.xml', express.static(path.join(__dirname, 'static/sitemap.xml')));
 app.use('/robots.txt', express.static(path.join(__dirname, 'static/robots.txt')));
 
-// Route for home
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'src', 'wip.html'));
-// });
+// Load partials (nav/footer) once at startup
+let navHtml = '';
+let footerHtml = '';
+let headHtml = '';
+try {
+  navHtml = fs.readFileSync(path.join(__dirname, 'src', 'partials', 'nav.html'), 'utf8');
+} catch (err) {
+  console.warn('nav partial not found:', err.message);
+}
+try {
+  footerHtml = fs.readFileSync(path.join(__dirname, 'src', 'partials', 'footer.html'), 'utf8');
+} catch (err) {
+  console.warn('footer partial not found:', err.message);
+}
+try {
+  headHtml = fs.readFileSync(path.join(__dirname, 'src', 'partials', 'analytics.html'), 'utf8');
+} catch (err) {
+  console.warn('analytics partial not found:', err.message);
+}
+
+function renderPage(res, pageFilePath) {
+  fs.readFile(pageFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading page:', pageFilePath, err);
+      return res.status(500).send('Erreur serveur');
+    }
+
+    // Insert analytics/head partial before closing </head>
+    let out = data.replace(/<\/head>/i, headHtml + '\n</head>');
+
+    // Insert nav after the opening <body> tag
+    out = out.replace(/<body([^>]*)>/i, (match) => {
+      return match + '\n' + navHtml;
+    });
+
+    // Insert footer before closing </body>
+    out = out.replace(/<\/body>/i, footerHtml + '\n</body>');
+
+    res.send(out);
+  });
+}
+
+// Routes using renderPage so nav/footer partials are injected
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+  renderPage(res, path.join(__dirname, 'src', 'index.html'));
 });
 
 app.get('/trousse-secours', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'trousse-secours.html'));
+  renderPage(res, path.join(__dirname, 'src', 'trousse-secours.html'));
 });
 
 app.get('/sacs-evacuation', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'sacs-evacuation.html'));
+  renderPage(res, path.join(__dirname, 'src', 'sacs-evacuation.html'));
 });
 
 app.get('/categories', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'categories.html'));
+  renderPage(res, path.join(__dirname, 'src', 'categories.html'));
 });
 
 app.get('/batteries', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'batteries.html'));
+  renderPage(res, path.join(__dirname, 'src', 'batteries.html'));
 });
 
 app.get('/reserves-alimentaires', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'reserves-alimentaires.html'));
+  renderPage(res, path.join(__dirname, 'src', 'reserves-alimentaires.html'));
 });
 
 app.get('/radios', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'radios.html'));
+  renderPage(res, path.join(__dirname, 'src', 'radios.html'));
 });
 
 app.get('/divers', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'divers.html'));
+  renderPage(res, path.join(__dirname, 'src', 'divers.html'));
 });
 
 // Fallback for 404
